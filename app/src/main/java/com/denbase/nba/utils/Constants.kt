2ffcbc202998ce2abbc1f1f70107c5eb8a0fedca
Emre Şahin <emre10sahin10@gmail.com>
2021-11-03 20:59:35 +0300

    fix

--- app/src/main/java/com/denbase/nba/utils/Constants.kt
+++ app/src/main/java/com/denbase/nba/utils/Constants.kt
@@ -3,6 +3,5 @@ package com.denbase.nba.utils
 class Constants {
     companion object{
         val BASE_URL = "https://www.balldontlie.io/api/v1/"
-        val FILM_URL = "https://raw.githubusercontent.com/emresahin10/FilmlerAPIJson/main/"
     }
 }
